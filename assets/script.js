f7d52a75bb0f2cac54040ea4b1a6227b5d46d112
Kyle Jones <kjones901@gmail.com>
2023-03-18 16:24:08 -0600

    started initial html code

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+// Variables
+
+
+
+
+
+
+
+
+// init function
+
+
+
+
+
+
+// game function
+// if else with a variable containing correct answer. Wrong answer "else statement" removes time from clock, shakes red, removes answer. Only move on when correct answer is selected.
+
+
+
+
+
+
+
+
+//timer function
+
+
+
+
+
+//local storage high score function
+
+
+
+
+
+
